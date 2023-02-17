8fe93da6524c81f00469310e76d809edf537d83d
Emre Turhal <you@example.com>
2023-02-17 18:13:33 +0100

    - Markers for each estate added on Map - Intent to detailsactivity for each markers added

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/mapview/MapFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/mapview/MapFragment.kt
@@ -1,20 +1,23 @@
 package com.openclassrooms.realestatemanager.ui.mapview
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.model.Estate
 import com.openclassrooms.realestatemanager.model.FullEstate
+import com.openclassrooms.realestatemanager.ui.EstateDetailsActivity
 import com.openclassrooms.realestatemanager.view.EstateViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,8 +28,8 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private val estateViewModel : EstateViewModel by viewModels()
     private val estatesList : MutableList<FullEstate> = ArrayList()
-    private val estatesAddress = mutableListOf<LatLng>()
-
+   // private val estatesList = mutableListOf<FullEstate>()
+    // VOIR DIFF2RENCE ENTRE DIFF2RENTES types de listes
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,26 +44,47 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         estateViewModel.allEstates.observe(viewLifecycleOwner) { estates ->
-            estates.let { estatesList.addAll(it) }
+            estates.let { estatesList.addAll(estates) }
         }
 
-        for (i in estatesList){
-            estatesAddress.add(LatLng(i.estate.latitude, i.estate.longitude))
-        }
+       // for (i in estatesList){
+       //     estatesAddress.add(LatLng(i.estate.latitude, i.estate.longitude))
+    //    }
 
     }
 
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
+        mMap.setOnMarkerClickListener(this::onMarkerClick)
         // Add a marker in Sydney and move the camera
         //val sydney = LatLng(-34.0, 151.0)
-        for (i in estatesAddress){
-            mMap.addMarker(MarkerOptions().position(i).title("Marker in Sydney"))
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(i))
+        if (estatesList.isNotEmpty()){
+        for (i in estatesList.indices){
+            val latitude : Double = estatesList[i].estate.latitude
+            val longitude : Double = estatesList[i].estate.longitude
+            val locations = LatLng(latitude, longitude)
+            //val latLong = LatLng(estatesList[i].estate.latitude, estatesList[i].estate.longitude)
+         val marker: Marker? =  mMap.addMarker(MarkerOptions().position(locations).title(estatesList[i].estate.address))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(locations))
+            marker?.tag = estatesList[i].estate
+
+        }} else {
+            val sydney = LatLng(-34.0, 151.0)
+            mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
         }
 
 
+
     }
+
+     private fun onMarkerClick(marker: Marker): Boolean {
+
+        val intent = Intent(activity, EstateDetailsActivity::class.java)
+        intent.putExtra(EstateDetailsActivity.ESTATE, marker.tag as Estate)
+        startActivity(intent)
+        return false
+    }
+
 }
