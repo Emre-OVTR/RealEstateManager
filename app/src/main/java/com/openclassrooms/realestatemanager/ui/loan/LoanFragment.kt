b35c00394d86555b52f2321f933acec505c3d5af
Emre Turhal <you@example.com>
2023-02-21 18:03:06 +0100

    Loan Fragment Added with Loan simulator

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/loan/LoanFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/loan/LoanFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.databinding.FragmentLoanBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,42 +22,78 @@ private const val ARG_PARAM2 = "param2"
  */
 class LoanFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var fragmentLoanBinding: FragmentLoanBinding
+
+    //instancier textviews et button
+    // clcicklistener sur button qui declenche un calcul
+    // affichage des resultats
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        fragmentLoanBinding = FragmentLoanBinding.inflate(layoutInflater)
+        fragmentLoanBinding.loanCalculate.setOnClickListener {
+            calculate()
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loan, container, false)
+    ): View {
+        return fragmentLoanBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoanFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoanFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+
+
+    private fun calculate(){
+        var canCalculate = false
+        val amount = fragmentLoanBinding.loanAmount.text.toString().toDoubleOrNull() ?: 0.0
+        val downPayment = fragmentLoanBinding.loanDown.text.toString().toDoubleOrNull() ?: 0.0
+        val term = fragmentLoanBinding.loanTerm.text.toString().toDoubleOrNull()
+        val interest = fragmentLoanBinding.loanInterest.text.toString().toDoubleOrNull()
+
+        when{
+            fragmentLoanBinding.loanAmount.text.isNullOrEmpty() || fragmentLoanBinding.loanTerm.text.isNullOrEmpty() || fragmentLoanBinding.loanInterest.text.isNullOrEmpty() || downPayment >= amount!! -> {
+                canCalculate = false
+                if (fragmentLoanBinding.loanAmount.text.isNullOrEmpty()){
+                   fragmentLoanBinding.loanAmountLayout.error = resources.getString(R.string.loan_error)
+                }
+                if (fragmentLoanBinding.loanTerm.text.isNullOrEmpty()){
+                    fragmentLoanBinding.loanTermLayout.error = resources.getString(R.string.loan_error)
+                }
+                if (fragmentLoanBinding.loanInterest.text.isNullOrEmpty()){
+                    fragmentLoanBinding.loanInterestLayout.error = resources.getString(R.string.loan_error)
+                }else if (interest!! < 0 || interest > 100){
+                    fragmentLoanBinding.loanInterestLayout.error = resources.getString(R.string.loan_error_interest_value)
+                }
+                if (downPayment != null && downPayment >= amount!!){
+                    fragmentLoanBinding.loanDownLayout.error = resources.getString(R.string.loan_error_down_payment)
                 }
             }
+            else -> {
+                canCalculate = true
+                fragmentLoanBinding.loanAmountLayout.error = null
+                fragmentLoanBinding.loanTermLayout.error = null
+                fragmentLoanBinding.loanInterest.error = null
+                fragmentLoanBinding.loanDownLayout.error = null
+            }
+        }
+
+        if (canCalculate){
+            val result: Double
+            val totalPrice: Double
+            if(interest == 0.0){
+                result = (if (downPayment != null) ( amount!!-(downPayment)) else amount)!! / (term!! * 12)
+                totalPrice = 0.0
+            }else{
+                result = (if (downPayment != null) (amount!! -(downPayment)) else amount)!! * ((interest!! / (100)) / (12)) / (1 - Math.pow( 1 + ((interest / 100) / 12), -term!! *12))
+                totalPrice = 12 * term * result - (if (downPayment != null ) amount?.minus(downPayment)!! else amount!!)
+            }
+            fragmentLoanBinding.loanMonthly.setText(String.format("%.2f",result), TextView.BufferType.EDITABLE)
+            fragmentLoanBinding.loanTotal.setText(String.format("%.2f",totalPrice), TextView.BufferType.EDITABLE)
+
+        }
     }
 }
